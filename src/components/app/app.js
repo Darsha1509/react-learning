c7d2a9938cfc458cba0185170a26dc1f2aac01fb
darya <kokorina.darya1509@gmail.com>
2019-11-08 12:25:41 +0300

    add smth

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
-import ItemDetails from '../item-details';
+import ItemDetails, { Record } from '../item-details';
 import Row from '../row';
 
 import './app.css';
@@ -45,14 +45,21 @@ export default class App extends Component {
       <ItemDetails 
         itemId={11}
         getData={getPerson}
-        getImageUrl={getPersonImage} />
+        getImageUrl={getPersonImage}>
+        <Record field="gender" label="Gender" />
+        <Record field="eyeColor" label="Eye Color" />  
+      </ItemDetails>
     );
 
     const starshipDetails = (
       <ItemDetails 
         itemId={5} 
         getData={getStarship}
-        getImageUrl={getStarshipImage} />
+        getImageUrl={getStarshipImage}>
+        <Record field="model" label="Model" />
+        <Record field="length" label="Length" />
+        <Record field="costInCredits" label="Cost" /> 
+      </ItemDetails>
     );
 
     return (
--- src/components/item-details/item-details.js
+++ src/components/item-details/item-details.js
@@ -6,6 +6,15 @@ import SwapiService from '../../services/swapi-service';
 
 import './item-details.css';
 
+export const Record = ({ item, field, label }) => {
+  return (
+    <li className="list-group-item">
+      <span className="term">{label}</span>
+      <span>{item[field]}</span>
+    </li>
+  )
+};
+
 export default class ItemDetails extends Component {
 
   swapiService = new SwapiService();
@@ -53,7 +62,7 @@ export default class ItemDetails extends Component {
 
     const spinner = loading ? <Spinner /> : null;
 
-    const { id, name, gender, birthYear, eyeColor } = item;
+    const { name } = item;
 
     return(
       <div className="person-details card">
@@ -65,18 +74,11 @@ export default class ItemDetails extends Component {
         <div className="card-body">
           <h4>{name}</h4>
           <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>{gender}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year</span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color</span>
-              <span>{eyeColor}</span>
-            </li>
+            {
+              React.Children.map(this.props.children, (child) => {
+                return React.cloneElement(child, { item });
+              })
+            }
           </ul>
           <ErrorButton />
         </div>
